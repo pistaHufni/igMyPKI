8e7ee3ee138ba87ab3f18589e4810c09eaa9338a
igoo <igoo@192.168.0.14>
2016-01-20 16:12:39 +0100

    test 4

---
+++ src/org/igoo/test/igClassTest3.java
@@ -0,0 +1,23 @@
+package org.igoo.test;
+
+import org.igoo.genericClass.igGenericClass;
+
+public class igClassTest3 extends igGenericClass {
+
+	public igClassTest3(String[] args, String programName) {
+		super(args, programName);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
