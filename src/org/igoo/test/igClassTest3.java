692bcd7a148b18ce5df465d065a713081e89a2e3
igoo <igoo@192.168.0.14>
2016-01-20 16:17:16 +0100

    test - 5

--- src/org/igoo/test/igClassTest3.java
+++ src/org/igoo/test/igClassTest3.java
@@ -6,7 +6,6 @@ public class igClassTest3 extends igGenericClass {
 
 	public igClassTest3(String[] args, String programName) {
 		super(args, programName);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
